02750d56d45e4e3ab4ea3a14632e94a7f8dfd114
g-tumwesigye <g.tumwesigy@alustudent.com>
2023-10-02 05:48:51 -0700

    task6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,26 @@
 #!/usr/bin/node
+
 const request = require('request');
-const baseUrl = process.argv[2];
-let count = {};
-request(baseUrl, (error, response) => {
-  if (error) console.log(error);
-  const result = JSON.parse(response.body);
-  result.forEach((item) => {
-    if (item.completed == true) {
-      if (item.userId in count) {
-        count[item.userId] += 1;
-      } else {
-        count[item.userId] = 1;
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
-  });
-  console.log(count);
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
